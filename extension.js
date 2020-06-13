23e6882c10ba0d1a89fe25cdb593fb9f81303aea
Romain Théry <romain.thery@live.fr>
2020-06-13 21:14:37 +0200

    chore: remove debug logs

--- extension.js
+++ extension.js
@@ -28,30 +28,25 @@ const PowerSaveState = Object.freeze({
 });
 
 function init() {
-    log('init()');
+    log('init');
 }
 
 function enable() {
-    log('enable()');
+    log('enable');
 
     // We wait the network System Menu to be displayed
     const networkManagerApplet = Main.panel.statusArea['aggregateMenu']._network;
     NetworkManagerAppletOpenSignal[0] = networkManagerApplet.menu;
     NetworkManagerAppletOpenSignal[1] = NetworkManagerAppletOpenSignal[0].connect('open-state-changed', (menu, open) => {
-        log(`networkManagerApplet: ${open}`);
-
         if (open) {
             const wirelessDevices = networkManagerApplet._devices['wireless'].devices; // imports.ui.status.network.NMConnectionCategory.WIRELESS is causing warning because not declared as var
             if (wirelessDevices !== null) {
-                log(`Iterating on ${wirelessDevices.length} wirelessDevices`);
                 // For each wireless device, we listen when its submenu is displayed
                 for (let i = 0; i < wirelessDevices.length; i++) {
                     const wirelessDevice = wirelessDevices[i];
                     let separator;
                     let powerManagementToggle;
                     const wirelessDeviceOpenSignalId = wirelessDevice.item.menu.connect('open-state-changed', (wirelessDeviceMenu, wirelessDeviceMenuOpen) => {
-                        log(`${wirelessDevice._device.get_iface()}: ${wirelessDeviceMenuOpen}`);
-
                         if (wirelessDeviceMenuOpen) {
                             const iface = wirelessDevice._device.get_iface();
                             const currentPowerManagementState = getPowerManagementState(iface);
@@ -82,7 +77,6 @@ function enable() {
                             powerManagementToggle = new PopupMenu.PopupSwitchMenuItem('Power Management', currentState);
                             PowerManagementToggleSignal[0] = powerManagementToggle;
                             PowerManagementToggleSignal[1] = powerManagementToggle.connect('toggled', toggle => {
-                                log(`toggle.state: ${toggle._switch.state}`);
                                 GLib.spawn_command_line_sync(`bash -c "nmcli connection modify id "${connectionName}" 802-11-wireless.powersave ${toggle._switch.state ? '3' : '2'}"`);
                                 GLib.spawn_command_line_sync(`bash -c "nmcli connection down id "${connectionName}""`);
                                 GLib.spawn_command_line_async(`bash -c "nmcli --wait 1 connection up id "${connectionName}""`);
@@ -99,7 +93,6 @@ function enable() {
                     });
 
                     WirelessDeviceOpenSignals.push([wirelessDevice.item.menu, wirelessDeviceOpenSignalId]);
-                    log(`wirelessDeviceOpenSignals[${i}]: ${wirelessDeviceOpenSignalId}`);
                 }
             }
         } else {
@@ -107,22 +100,18 @@ function enable() {
                 const wirelessDeviceOpenSignal = WirelessDeviceOpenSignals.pop();
                 wirelessDeviceOpenSignal[0].disconnect(wirelessDeviceOpenSignal[1]);
                 wirelessDeviceOpenSignal[0] = null;
-                log(`disconnect wirelessDeviceOpenSignal ${wirelessDeviceOpenSignal[1]}`);
             }
 
             destroyPowerManagementItems();
         }
     });
-
-    log(`networkManagerAppletOpenSignal: ${NetworkManagerAppletOpenSignal[1]}`);
 }
 
 function disable() {
-    log('disable()');
+    log('disable');
 
     NetworkManagerAppletOpenSignal[0].disconnect(NetworkManagerAppletOpenSignal[1]);
     NetworkManagerAppletOpenSignal[0] = null;
-    log(`disconnect networkManagerAppletOpenSignal ${NetworkManagerAppletOpenSignal[1]}`);
 
     destroyPowerManagementItems();
 }
@@ -132,7 +121,6 @@ function getPowerManagementState(iface) {
     let [ok, out, err, exit] = GLib.spawn_command_line_sync(`bash -c "iwconfig ${iface} | grep 'Power Management'"`);
     if (out.length) {
         let powerManagementLine = ByteArray.toString(out);
-        log(`getPowerManagementState: ${powerManagementLine}`);
         if (powerManagementLine.endsWith(':on\n'))
             return PowerManagementState.ENABLED;
 
@@ -147,7 +135,6 @@ function getConnectionName(iface) {
     let [ok, out, err, exit] = GLib.spawn_command_line_sync(`bash -c "nmcli -g GENERAL.CONNECTION device show ${iface}"`);
     if (out.length) {
         let connectionName = ByteArray.toString(out).trim();
-        log(`getConnectionName: ${connectionName}`);
         return connectionName;
     }
 
@@ -160,7 +147,6 @@ function getPowerSaveState(connectionName) {
         let [ok, out, err, exit] = GLib.spawn_command_line_sync(`bash -c "nmcli -g 802-11-wireless.powersave connection show id "${connectionName}""`);
         if (out.length) {
             let powerSaveState = ByteArray.toString(out);
-            log(`getPowerSaveState: ${powerSaveState}`);
             return powerSaveState;
         }
     }
